e639c40847c536bef41cf5b112e800da7004e3f1
usman611b <usmanali611b@gmail.com>
2026-08-11 23:03:57 +0500

    Day-15 session06

---
+++ math-for-ai/day-16/session06.py
@@ -0,0 +1,31 @@
+# Ordinary Gradient Descent
+x_gd = 5
+learning_rate = 0.1
+
+print("Ordinary Gradient Descent")
+
+for step in range(5):
+    gradient = 2 * x_gd
+    x_gd = x_gd - learning_rate * gradient
+
+    print("Step:", step + 1, "x:", round(x_gd, 4))
+
+
+# Gradient Descent with Momentum
+x_momentum = 5
+velocity = 0
+momentum = 0.9
+
+print("\nMomentum")
+
+for step in range(5):
+    gradient = 2 * x_momentum
+
+    velocity = momentum * velocity - learning_rate * gradient
+    x_momentum = x_momentum + velocity
+
+    print(
+        "Step:", step + 1,
+        "x:", round(x_momentum, 4),
+        "velocity:", round(velocity, 4),
+    )
